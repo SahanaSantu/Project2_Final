c64ffe921757f3afba1e0af024993b05beb6f0eb
Sahana Umashankar <su83@afsconnect2.njit.edu>
2017-12-01 15:07:21 -0500

    db conn file is added

---
+++ core/database/dbConn.php
@@ -0,0 +1,36 @@
+<?php
+Namespace DbConnection;
+use \PDO;
+
+//DB connection
+class dbConn{
+
+    //variable to hold connection object.
+    protected static $db;
+    //private construct - class cannot be instatiated externally.
+    private function __construct() {
+        try {
+            // assign PDO object to db variable
+            self::$db = new PDO(HOST,USERNAME,PASSWORD);
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            //echo "Connection success: ";
+            
+        }
+        catch (PDOException $e) {
+            //Output error - would normally log this to error file rather than output to user.
+            echo "Connection Error: ";
+        }
+    }
+    // get connection function. Static method - accessible without instantiation
+    public static function getConnection() {
+        //Guarantees single instance, if no connection object exists then create one.
+        if (!self::$db) {
+            //new connection object.
+            new dbConn();
+        }
+        //return connection.
+        return self::$db;
+    }
+}
+
+?>
