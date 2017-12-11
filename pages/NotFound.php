1b6c1bb54775550769b86919b3968cb0b8b1cea4
SahanaSantu <su83@njit.edu>
2017-12-10 22:12:23 -0500

    Not Found page added

---
+++ pages/NotFound.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Final project</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+
+
+
+<body style="background-image:url(https://www.taurho-transcribes.co.uk/wp-content/uploads/2017/02/Login-background-image-resized.png)">
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  <strong><?php echo "404 Not Found"?></strong>
+      
+        </div>
+    </divd>
+</div>
+
+
+</body>
+</html>
